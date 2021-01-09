abfbdeb3d3036ef7075b634e6324df56559e3cd8
Daniele <d.tarini@hyperborea.com>
2021-01-09 14:36:33 +0100

    Working json broadcast

---
+++ sse-games-rest/src/main/java/it/dantar/games/sse/SseController.java
@@ -0,0 +1,34 @@
+package it.dantar.games.sse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@RestController
+@CrossOrigin
+public class SseController {
+
+	@Autowired
+	SseService sseService;
+	
+	@GetMapping("/alive")
+	public Boolean alive() {
+		return true;
+	}
+
+	@GetMapping("/games/{gameId}/sse")
+	public SseEmitter playerSse(@PathVariable String gameId) {
+		return sseService.sse(gameId);
+	}
+
+	@PostMapping("/games/{gameId}/json")
+	public void postMessage(@PathVariable String gameId, @RequestBody Object message) {
+		sseService.broadcastJson(gameId, message);
+	}
+
+}
---
+++ sse-games-rest/src/main/java/it/dantar/games/sse/SseService.java
@@ -0,0 +1,82 @@
+package it.dantar.games.sse;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.http.MediaType;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@Service
+public class SseService {
+
+	@Autowired
+	ApplicationContext context;
+	
+	private static final Long TIMEOUT = 1000 * 60 * 60L; // 1 ora
+
+	private Map<String, List<SseEmitter>> emitters = new HashMap<>();
+
+	static final private Map<String, String> PING = new HashMap<String, String>();
+	static {
+		PING.put("code", "ping");
+	}
+	
+	@Scheduled(fixedDelayString = "${service.ping.delay}")
+	public void refreshSse() {
+		try {
+			this.emitters.keySet().stream()
+			.forEach(k -> this.broadcastJson(k, PING));			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+
+	public void broadcastJson(String gameId, Object data) {
+		this.emitters.get(gameId).forEach(emitter -> {
+			try {
+				Logger.getLogger(this.getClass().getName()).warning(String.format("Broadcast to SSE %s in game %s (%s).", 
+						emitter.toString(), gameId, this.emitters.get(gameId).toString()));				
+				emitter.send(SseEmitter.event().data(data, MediaType.APPLICATION_JSON));
+			} catch (IOException e) {
+				// emitter.completeWithError(e);
+				Logger.getLogger(this.getClass().getName()).warning(String.format("\tERROR for SSE %s in game %s. Exception message: %s", 
+						emitter.toString(), gameId, e.getMessage()));
+			}
+		});
+	}
+
+	public SseEmitter sse(String gameId) {
+		Logger.getLogger(this.getClass().getName()).info(String.format("SSE request: %s", gameId));
+		if (!emitters.containsKey(gameId)) {
+			Logger.getLogger(this.getClass().getName()).info(String.format("\t game %s does not exist: create game", gameId));
+			emitters.put(gameId, new ArrayList<>());
+		}
+		SseEmitter sse = new SseEmitter(TIMEOUT);
+		sse.onError(error -> {
+			Logger.getLogger(this.getClass().getName()).info(String.format("SSE error for game %s (%s): removing SSE %s %s", 
+					gameId, this.emitters.get(gameId).toString(), this.emitters.get(gameId).indexOf(sse), sse.toString()));
+			Boolean done = this.emitters.get(gameId).remove(sse);
+			Logger.getLogger(this.getClass().getName()).info(String.format("\tremoval done: %s", done));
+			sse.completeWithError(new RuntimeException("SSE client error"));
+		});
+		sse.onCompletion(() -> {
+			Logger.getLogger(this.getClass().getName()).info(String.format("SSE completion in game %s (%s): SSE %s %s", 
+					gameId, this.emitters.get(gameId).toString(), this.emitters.get(gameId).indexOf(sse), sse.toString()));
+		});
+		sse.onTimeout(() -> {
+			Logger.getLogger(this.getClass().getName()).info(String.format("SSE timeout in game %s (%s): SSE %s %s", 
+					gameId, this.emitters.get(gameId).toString(), this.emitters.get(gameId).indexOf(sse), sse.toString()));
+		});
+		emitters.get(gameId).add(sse);
+		return sse;
+	}
+
+}
